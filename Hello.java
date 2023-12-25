ca2690ba6a5cebde17076038ec04e38000d56ea1
D-Mahaprabu <prabhuvasanth2000@gmail.com>
2023-12-25 12:30:52 +0530

    emergency fixed

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ class Hello {
     public static void main(String[] args){
         System.out.print("2nd");
         System.out.print("without staging");
+        System.out.print("branching");
+        System.out.print("emergency");
     }
 }
